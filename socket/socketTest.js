8f7ee62bd4c07f51d39bf592acfe2445844a99a3
Kevin Lin <stuy.kevinlin@gmail.com>
2022-02-28 16:17:11 -0500

    Socket test server

---
+++ socket/socketTest.js
@@ -0,0 +1,11 @@
+const SOCKET_URL = 'ws://localhost:8000';
+const socket = new WebSocket(SOCKET_URL);
+
+socket.addEventListener('open', function (event) {
+    var newEvent = {type: 'connected', data: 'Howdy'};
+    socket.send(JSON.stringify(newEvent));
+});
+
+socket.addEventListener('message', function (event) {
+    console.log('Message from server ', JSON.parse(event.data));
+});
