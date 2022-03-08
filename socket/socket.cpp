92926eb9096da50ef4b29471627f2f8f99519a87
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2022-03-07 23:46:14 -0500

    socket fix

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -1,10 +1,11 @@
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
-#include <boost>
+ 
 #include <iostream>
 #include <string>
  
-typedef websocketpp::client<websocketpp::config::asio_client> client; 
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+ 
 int main() {
     bool done = false;
     std::string input;
