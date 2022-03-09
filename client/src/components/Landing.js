3855151d6f7bed7aeaf301e906914ce542566380
Rachel Ng <rachel.ng.135@gmail.com>
2022-03-09 16:41:15 -0500

    landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+
+
+function GamePlayModeButtons(prop) {
+    return (
+        <Box container sx={{ justifyContent: 'space-around', alignContent: 'flex-start' }} >
+            <Button variant="contained" sx={{ m: 1 }} >Tutorial</Button>
+            <Button variant="contained" sx={{ m: 1 }} >Join Game</Button>
+            <Button variant="contained" sx={{ m: 1 }} >Private Party</Button>
+            <TextField id="outlined-basic" variant="outlined" size="small" sx={{ m: 1 }} 
+                label="Private Party" />
+        </Box>
+    );
+
+}
+
 
 function Landing() {
 
     return (
-        <Container sx={{ width: '100%', justifyContent: 'center', alignContent: 'center' }}> 
-            <Button variant="contained">Tutorial</Button>
-            <Button variant="contained">Join Game</Button>
-            <Button variant="contained">Private Party</Button>
-        </Container>
+
+        <Grid container sx={{ justifyContent: 'space-around', alignContent: 'flex-start', 
+            width: '100%' }}>
+            <GamePlayModeButtons />
+        </Grid>
     );
 }
 
