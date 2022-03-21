022945d0f31c5c921ca1fb96246fbf4ef82dca3a
jcruse <jack.cruse@enertiv.com>
2022-03-21 15:29:08 -0400

    changed game to class

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -14,37 +14,39 @@ import Player from './Player';
 
 
 
-function Game(props) {
-    // 99 - 1 = 98 / 2 = 49 
-    let numPlayers = (31 - 1)/2;
+class Game extends React.Component {
+    render() {
+        // 99 - 1 = 98 / 2 = 49 
+        let numPlayers = (31 - 1)/2;
 
 
-    return (
-        <Grid container sx={{ justifyContent: 'space-around', alignContent: 'flex-start', 
-            width: '100%' }}>
+        return (
+            <Grid container sx={{ justifyContent: 'space-around', alignContent: 'flex-start', 
+                width: '100%' }}>
 
-            <Box sx={{ width: 1/5 }}>
-                <Box sx={{ height: 90 }} />
+                <Box sx={{ width: 1/5 }}>
+                    <Box sx={{ height: 90 }} />
 
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-                    {Array.from(new Array(numPlayers)).map((item, index) => (<Player />))}
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+                        {Array.from(new Array(numPlayers)).map((item, index) => (<Player />))}
+                    </Box>
                 </Box>
-            </Box>
 
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignContent: 'center' }}>
-                <Puzzle />
-            </Box>
-
-            <Box sx={{ width: 1/5 }}>
-                <Box sx={{ height: 90 }} />
-
-                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
-                    {Array.from(new Array(numPlayers)).map((item, index) => (<Player />))}
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignContent: 'center' }}>
+                    <Puzzle />
                 </Box>
-            </Box>
 
-        </Grid>
-    );
+                <Box sx={{ width: 1/5 }}>
+                    <Box sx={{ height: 90 }} />
+
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+                        {Array.from(new Array(numPlayers)).map((item, index) => (<Player />))}
+                    </Box>
+                </Box>
+
+            </Grid>
+        );
+    };
 }
 
 export default Game; 
