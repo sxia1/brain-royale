fbc794f5a6e2b56c9fdd6d78beb054285a940f98
Rachel Ng <rachel.ng.135@gmail.com>
2022-03-28 15:29:55 -0400

    colored buttons

--- client/src/components/ColorMatch.js
+++ client/src/components/ColorMatch.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom';
 
 import Button from '@mui/material/Button';
 
+import { red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, orange, deepOrange, brown, grey, blueGrey } from '@mui/material/colors';
+
 //to use: <ColorMatch handler={handleSolve}>
 function ColorMatch(props){
   //const [solved, setSolved] = React.useState(-1); //-1: not acted, 0: incorrect, 1: correct
-  const colors = ["error", "warning", "success", "primary", "secondary"];
-  const words = ["Red", "Orange", "Green", "Blue", "Purple"];
+  // const colors = ["error", "warning", "success", "primary", "secondary"];
+  const colors = [red, orange, yellow, green, blue, purple];
+  const words = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
   const types = ["Word", "Color"];
   let type = Math.floor(Math.random()*2);
   
-  let colorA = Math.floor(Math.random()*5);
-  let colorB = Math.floor(Math.random()*4);
+  let colorA = Math.floor(Math.random()*6);
+  let colorB = Math.floor(Math.random()*5);
   if(colorB >= colorA){
     colorB ++;
   }
@@ -27,8 +30,8 @@ function ColorMatch(props){
       <br></br>
       <h1>Choose {types[type]} {targetWords[target]}</h1>
       <br></br>
-      <Button variant="outlined" size="large" color={colors[colorA]}>{words[colorB]}</Button>
-      <Button variant="outlined" size="large" color={colors[colorB]}>{words[colorA]}</Button>
+      <Button variant="outlined" size="large" variant="contained" sx={{ backgroundColor: colors[colorA][500] }}>{words[colorB]}</Button>
+      <Button variant="outlined" size="large" variant="contained" sx={{ backgroundColor: colors[colorB][500] }}>{words[colorA]}</Button>
     </div>
   );
 }
