db83325d306355624831feba8fb3799f7d7ed3c1
Rachel Ng <rachel.ng.135@gmail.com>
2022-03-28 15:45:23 -0400

    visuals

--- client/src/components/ColorMatch.js
+++ client/src/components/ColorMatch.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 
 import { red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, green, lightGreen, lime, yellow, amber, orange, deepOrange, brown, grey, blueGrey } from '@mui/material/colors';
@@ -9,7 +11,7 @@ import { red, pink, purple, deepPurple, indigo, blue, lightBlue, cyan, teal, gre
 function ColorMatch(props){
   //const [solved, setSolved] = React.useState(-1); //-1: not acted, 0: incorrect, 1: correct
   // const colors = ["error", "warning", "success", "primary", "secondary"];
-  const colors = [red, orange, yellow, green, blue, purple];
+  const colors = [red[500], orange[500], yellow[600], green[500], blue[500], purple[500]];
   const words = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
   const types = ["Word", "Color"];
   let type = Math.floor(Math.random()*2);
@@ -27,11 +29,17 @@ function ColorMatch(props){
 
   return (
     <div>
-      <br></br>
-      <h1>Choose {types[type]} {targetWords[target]}</h1>
-      <br></br>
-      <Button variant="outlined" size="large" variant="contained" sx={{ backgroundColor: colors[colorA][500] }}>{words[colorB]}</Button>
-      <Button variant="outlined" size="large" variant="contained" sx={{ backgroundColor: colors[colorB][500] }}>{words[colorA]}</Button>
+        <Typography variant="h6">Choose {types[type]} {targetWords[target]}</Typography>
+
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-evenly' }}>
+            <Box sx={{ backgroundColor: colors[colorA], color: '#ffffff',  width: 1/3, m: 1, p: 2, textAlign: 'center' }}>
+                {words[colorB]}
+            </Box>
+
+            <Box sx={{ backgroundColor: colors[colorB], color: '#ffffff', width: 1/3, m: 1, p: 2, textAlign: 'center' }}>
+                {words[colorA]}
+            </Box>
+        </Box>
     </div>
   );
 }
