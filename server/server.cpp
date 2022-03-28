9fef2d230952d5ca72cc044829c78c4ecdc0c832
sxia1 <33487759+sxia1@users.noreply.github.com>
2022-03-27 22:29:20 -0400

    async webserver

--- server/server.cpp
+++ server/server.cpp
@@ -7,58 +7,69 @@
 #include <cstring>
 #include <string>
 #include <iostream>
+#include <future>
 #include "Router.h"
 #include "static_file.h"
 
 #define PORT std::stoi(getenv("PORT"))
 
-int main(int argc, char const *argv[]){
-    std::cout << "port: " << PORT << "\n";
-
+class http_server{
+public:
     struct sockaddr_in address;
-    int opt = 1;
     int server_fd;
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-     
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 3) < 0){
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-    std::cout << "port: " << PORT << "\n";
+    int opt = 1;
     Router router = Router();
-    while(1){
+    
+    http_server(){
+        // Creating socket file descriptor
+        if ((this->server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+         
+        // Forcefully attaching socket to the port 8080
+        if (setsockopt(this->server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &this->opt, sizeof(this->opt))){
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        this->address.sin_family = AF_INET;
+        this->address.sin_addr.s_addr = INADDR_ANY;
+        this->address.sin_port = htons( PORT );
+        // Forcefully attaching socket to the port 8080
+        if (bind(this->server_fd, (struct sockaddr *)&this->address, sizeof(this->address))<0){
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(this->server_fd, 3) < 0){
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+    }
+    void handle_connection(){
         int new_socket, valread;
-        int addrlen = sizeof(address);
+        int addrlen = sizeof(this->address);
         char buffer[1024] = {0};
-          
-        if ((new_socket = accept(server_fd, (struct sockaddr*) &address, (socklen_t*) &addrlen)) < 0){
+        if ((new_socket = accept(this->server_fd, (struct sockaddr*) &this->address, (socklen_t*) &addrlen)) < 0){
             perror("accept");
             exit(EXIT_FAILURE);
         }
-        valread = read( new_socket , buffer, 1024);
+        valread = read(new_socket, buffer, 1024);
         std::cout << buffer << std::endl;
-        char * response = router.respond(buffer);
+        char * response = this->router.respond(buffer);
         std::cout << "response found\n";
         send(new_socket, response, strlen(response), 0);
         printf("Hello message sent\n");
         close(new_socket);
     }
-    
+    void async_handle_connection(){
+        while(1){
+            std::async(&http_server::handle_connection, this);
+        }
+    }
+};
+
+int main(int argc, char const *argv[]){
+    http_server server;
+    server.async_handle_connection();
+    return 0;
 }
