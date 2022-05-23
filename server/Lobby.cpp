60ff554ba264e01018dd1c76c811fa5ca0f08fb9
jcruse <jack.cruse@enertiv.com>
2022-05-22 22:21:56 -0400

    added general logic to generate_new_puzzle

--- server/Lobby.cpp
+++ server/Lobby.cpp
@@ -49,20 +49,89 @@ bool Lobby::attack_player(const int attacker_id, const int reciever_id){
     return false;
 }
 
-// json Lobby::get_player_stats(const int player_id){
-//     Player_Stats player_info = this->player_list[player_id];
+json Lobby::get_player_stats(const int player_id){
+    Player_Stats player_info = this->player_list[player_id];
 
-//     json player = player_info;
+    json player = player_info;
 
-//     return player;
-// }
+    return player;
+}
 
-// json Lobby::get_player_stats(){
-//     json players;
-//     return player_list;
-// }
+json Lobby::get_player_stats(){
+    json players = player_list; 
+    return player_list;
+}
+
+/*
+output json should be of form:
+{
+    correct: either "left" or "right" corresponding to correct side
+    puzzle_type: either "word" or "color"
+    correct_word: correct word value corresponding to answer
+    left_word: word to be used on left
+    left_color: color to be used on left
+    right_word: word to be used on right
+    right_color: color to be used on right
+}
+
+code below generates values random for json in javascript 
+code just needs to be translated
+*/
+void Lobby::generate_new_puzzle(){
+    let type = types[Math.floor(Math.random() * types.length)];
+    let correct_rand = Math.floor(Math.random() * types.length);
+
+        let correct = answers[correct_rand % 2];
+        let correct_color = colors[correct_rand];
+        let correct_word = words[correct_rand];
+        let left_color, right_color, left_word, right_word;
+
+        let incorrect_color = colors[Math.floor(Math.random()* colors.length)];
+        let incorrect_word = words[Math.floor(Math.random() * words.length)];
+
+        //note that 'incorrect' is a general misnomer as some matches can occur
+        //we only want to ensure that if we ask to choose the correct word or color
+        //that there is only one instance of the word or color in each respective case
+        while (type == "word" && incorrect_word == correct_word) {
+            incorrect_word = words[Math.floor(Math.random() * words.length)];
+        }
+
+        while (type == "color" && incorrect_color == correct_color) {
+            incorrect_color = colors[Math.floor(Math.random()* colors.length)];
+        }
 
 
+        if (type == "word") {
+            if (correct == "left") {
+                left_word = correct_word;
+                right_color = correct_color;
+                right_word = incorrect_word;
+                left_color = incorrect_color;
+            }
+            else {
+                right_word = correct_word;
+                left_color = correct_color;
+                left_word = incorrect_word;
+                right_color = incorrect_color;
+            }
+        }
+
+        else {
+            if (correct == "left") {
+                left_color = correct_color;
+                right_word = correct_word;
+                right_color = incorrect_color;
+                left_word  = incorrect_word;
+            }
+
+            else {
+                right_color = correct_color;
+                left_word = correct_word;
+                left_color = incorrect_color;
+                right_word = incorrect_word;
+            }
+;
+}
 
 
 void Lobby::generate_new_puzzle(){
