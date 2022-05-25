03fbcf7dd0bb3c0169a587bd19d3b3e4e4227ef9
Mohammed Uddin <uddin.t.mohammed@gmail.com>
2022-05-24 23:25:02 -0400

    hard override

--- server/Lobby.h
+++ server/Lobby.h
@@ -11,53 +11,51 @@ typedef websocketpp::server<websocketpp::config::core> server;
 class iostream_server;
 
 struct Player_Stats {
-    int score; 
-    int time_left;
-    int puzzles_won;
-    int incorrect_solutions;
-    int total_attacks;
-    int total_times_attacked;
-    bool left_game;
+    int score = 0;
+    int time_left = 0;
+    int correct_solutions = 0;
+    int incorrect_solutions = 0;
+    int total_attacks = 0;
+    int total_times_attacked = 0;
+    int attacks_left = 0;
+    bool left_game = false;
+
 };
 
-// void to_json(json& j, const Player_Stats& player_info){
-//     j["score"] = player_info.score;
-//     j["time_left"] = player_info.time_left;
-//     j["puzzles_won"] = player_info.puzzles_won;
-//     j["incorrect_solutions"] = player_info.incorrect_solutions;
-//     j["total_attacks"] = player_info.total_attacks;
-//     j["total_times_attacked"] = player_info.total_times_attacked;
-//     j["left_game"] = player_info.left_game;
-// };
 
 class Lobby{
 private:
     std::unordered_map< int, websocketpp::connection_hdl > lobby;
     server *s;    
+    std::unordered_map<int, Player_Stats> player_list;
+    std::unordered_map<int, std::string> solutions;
+
     std::stringstream *output;
 public:
-
-    static int lobby_id;
-    std::vector<Player_Stats> player_list;
+    const int lobby_id = 1;
+    int lobby_size = 0;
+    bool started;
     bool is_private;
     int time_left; 
     int total_puzzles;
 
     Lobby();
     
-    Lobby(const bool is_private, const std::vector<Player_Stats> & player_list);
-    void add(int, websocketpp::connection_hdl, server*,std::stringstream *);
-  /*  
-    bool add_player(const Player_Stats & player);
+    // Lobby(const bool is_private, const std::vector<Player_Stats> & player_list);
+    void add(int, websocketpp::connection_hdl, server*);
+    void start_lobby();
+    
+    bool add_player(const Player_Stats & player, int socketnum);
     void deactivate_player(const int player_id);
     bool attack_player(const int attacker_id, const int reciever_id);
-     json get_player_stats(const int player_id);
-     json get_player_stats();
-    void generate_new_puzzle();
-    void verify_puzzle_solution();
+    // json get_player_stats(const int player_id);
+    // json get_player_stats();
+    json generate_new_puzzle(int socketnum);
     void skip_puzzle();
+    json verify_puzzle_solution(int socketnum, std::string solution);
     int size();
-*/
-    void attack();
-    void sendall();
+
+    json attack(int socketnum);
+    void sendall(json message);
+    
 };
--- server/iostream_server.cpp
+++ server/iostream_server.cpp
@@ -14,7 +14,7 @@ using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
 
-void iostream_on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg, LobbyController* lcontrol) {
+void iostream_on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg, LobbyController* lcontrol, int socketnum) {
     std::cout << "Hi, this humble on_message function was called!!!" << std::endl;
     if (msg->get_opcode() == websocketpp::frame::opcode::text) {
         s->get_alog().write(websocketpp::log::alevel::app,
@@ -43,53 +43,110 @@ void iostream_on_message(server* s, websocketpp::connection_hdl hdl, message_ptr
             "type" : "connect",
             "data" : null
         })"_json;
-        std::cout << "message opcode is " << msg->get_opcode() << "\n";
-        lcontrol->getList()[0]->sendall();
+        std::cout << "message optcode is" << msg->get_opcode() << "\n";
+        s->send(hdl, response.dump(), msg->get_opcode());
     }
-    
-   else if(responseType == "joinLobby"){
-        // if(lobbies.size() == 0){
-        //     std::vector<Player_Stats> players = {};
-        //     Lobby current = Lobby(false, players);
-        //     lobbies.push_back(current);
 
-        //     json response;
-        //     response["lobby_id"] = 1;
+   else if(responseType == "startLobby"){
+        std::vector<Lobby *> lobbies = lcontrol->getList();
+        int lobby_number = message["lobby_id"];
+        bool found = false;
+        json response = R"({
+            "type" : "startLobby",
+            "data" : "success"
+        })"_json;
 
-        //     s->send(hdl, response.dump(), websocketpp::frame::opcode::text);
+        for(Lobby* lobby: lobbies){
+            if(lobby->lobby_id == lobby_number){
+                lobby->started = true;
+                found = true;
+                lobby->sendall(response);
+                break;
+            }
+        }
+        response["data"] = "lobby does not exist";
 
-        // }
-        1;
+        if(not found){
+            s->send(hdl, response.dump(), msg->get_opcode());
+        }    
    }
-/*
-      
-        // Lobby()
-        // if(message.contains("data")){
-            // if(!typeid(message["data"]).name() == 'i'){
-            //     1;
-            // }
-            // std::string data = message["data"];
-        // }
-    }
-*/
 
-    else if(responseType == "completePuzzle"){
-        1;
-    }
-    else if(responseType == "skipPuzzle"){
-        1;
-    }
-    else if(responseType == "changeAttackStyle"){
-        1;
-    }
-    else if (responseType == "attack"){
-        lcontrol->getList()[0]->attack();
-    }
+   else if(responseType == "joinLobby"){
+        std::vector<Lobby *> lobbies = lcontrol->getList();
+        int lobby_number = message["lobby_id"];
+        bool found = false;
+        json response = R"({
+            "type" : "joinLobby",
+            "data" : "success"
+        })"_json;
+
+        for(Lobby* lobby: lobbies){
+            if(lobby->lobby_id == lobby_number){
+                lobby->add(socketnum, hdl, s);
+                Player_Stats player;
+                lobby->add_player(player, socketnum);
+                response["numUsers"] = lobby->lobby_size;
+                s->send(hdl, response.dump(), msg->get_opcode());
+                found = true;
+                break;
+            }
+        }       
+
+        response["data"] = "lobby does not exist";
+        if(not found){
+            s->send(hdl, response.dump(), msg->get_opcode());
+        }   
+   }
+
+   else if(responseType == "generateNewPuzzle"){
+        std::vector<Lobby *> lobbies = lcontrol->getList();
+        int lobby_number = message["lobby_id"];
+        bool found = false;
+        for(Lobby* lobby: lobbies){
+            if(lobby->lobby_id == lobby_number){
+                json puzzle = lobby->generate_new_puzzle(socketnum);
+                s->send(hdl, puzzle.dump(), msg->get_opcode());
+                found = true;
+                break;
+            }
+        }
+   }
+
+   else if(responseType == "verifyPuzzle"){
+       std::vector<Lobby *> lobbies = lcontrol->getList();
+        int lobby_number = message["lobby_id"];
+        std::string solution = message["side"];
+        bool found = false;
+
+        for(Lobby* lobby: lobbies){
+            if(lobby->lobby_id == lobby_number){
+                json response = lobby->verify_puzzle_solution(socketnum, solution);
+                s->send(hdl, response.dump(), msg->get_opcode());
+                found = true;
+                break;
+            }
+        }
+   }
+
+    else if(responseType == "attack"){
+       std::vector<Lobby *> lobbies = lcontrol->getList();
+        int lobby_number = message["lobby_id"];
+        bool found = false;
+
+        for(Lobby* lobby: lobbies){
+            if(lobby->lobby_id == lobby_number){
+                json response = lobby->attack(socketnum);
+                s->send(hdl, response.dump(), msg->get_opcode());
+                found = true;
+                break;
+            }
+        }
+   }
+
 }
 
 void on_open(server* s, websocketpp::connection_hdl hdl, int socketnum, LobbyController* lcontrol,std::stringstream *output){
     std::cout << "in open\n";
- //   lcontrol.debug();
     lcontrol->add(socketnum, hdl, s, output);
     
 }
@@ -115,7 +172,7 @@ iostream_server::iostream_server(std::stringstream *output, int socketnum, Lobby
         s.set_open_handler(std::bind(&on_open, &s, std::placeholders::_1, socketnum, lcontrol, output));
         std::cout << "after open\n";
         s.set_close_handler(std::bind(&on_close, &s, std::placeholders::_1));
-        s.set_message_handler(std::bind(&iostream_on_message,&s,std::placeholders::_1,std::placeholders::_2,lcontrol));
+        s.set_message_handler(std::bind(&iostream_on_message,&s,std::placeholders::_1,std::placeholders::_2,lcontrol,socketnum));
         con = s.get_connection();
         con->start();
 }
